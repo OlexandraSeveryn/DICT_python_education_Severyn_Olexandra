1704963e4e4e0652a84f6affc42ce8d5a27356b5
OlexandraSeveryn <o.y.severyn@student.khai.edu>
2021-12-08 01:23:44 +0200

    hangman 5-th stage

--- Hangout/hangman.py
+++ Hangout/hangman.py
@@ -4,19 +4,33 @@ print("""HANGMAN
 The game will be available soon.""")
 
 word_list=["python", "java", "javascript"]
+guesses = 8
 
 word_select = random.choice(word_list)
+letter_list = set(word_select)
 
-hint = ''
-for i in range(0,len(word_select)):
-    if i < 3:
-        hint +=word_select[i]
-    else :
-        hint +='-'
+hint = list('-')*len(word_select)
+while True:
+    print("".join(hint))
+    write = input("Input a letter: > ")
 
-write = input("Guess the word "+hint+"> ")
-if write == word_select:
-    print("You survived!")
-else :
-    print("You lost!")
+    if len(write)>1 :
+        print("You have to write one symbol ")
+        continue
 
+    if write in letter_list :
+        letter_list.remove(write)
+
+        for i in range(len(word_select)) :
+            if word_select[i] == write :
+                hint[i] += write
+    else:
+        guesses -= 1
+        print("That letter doesn't appear in the word")
+
+    if len(letter_list) == 0 :
+        print("You survived!")
+        break
+    elif guesses==0:
+        print("You lost!")
+        break
