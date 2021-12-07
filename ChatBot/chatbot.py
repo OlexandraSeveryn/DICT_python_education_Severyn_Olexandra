1bc4ebf77790cbd65b9b670d670a8e3844ac66bc
OlexandraSeveryn <o.y.severyn@student.khai.edu>
2021-12-07 02:00:27 +0200

    ChatBot 2-nd stage

---
+++ ChatBot/chatbot.py
@@ -0,0 +1,7 @@
+def welcome():
+    print("Hello! My name is Bob_Bot")
+    print("I was created in 2021")
+    name=input("Please, remaind me your name:")
+    print("What a great name you have,", name)
+
+
