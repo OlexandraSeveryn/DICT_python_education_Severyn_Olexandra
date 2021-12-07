7b719e4685350739dac2d226a470f4dd83a00fda
OlexandraSeveryn <o.y.severyn@student.khai.edu>
2021-12-08 01:40:42 +0200

    hangman 6-th stage

--- Hangout/hangman.py
+++ Hangout/hangman.py
@@ -8,13 +8,14 @@ guesses = 8
 
 word_select = random.choice(word_list)
 letter_list = set(word_select)
+list_use_letter = set()
 
 hint = list('-')*len(word_select)
 while True:
     print("".join(hint))
     write = input("Input a letter: > ")
 
-    if len(write)>1 :
+    if len(write) >1 or len(write) == 0:
         print("You have to write one symbol ")
         continue
 
@@ -23,10 +24,16 @@ while True:
 
         for i in range(len(word_select)) :
             if word_select[i] == write :
-                hint[i] += write
+                hint[i] = write
     else:
         guesses -= 1
-        print("That letter doesn't appear in the word")
+        if write in list_use_letter:
+            print("No improvements")
+        else:
+            print("That letter doesn't appear in the word")
+
+    if write in word_select :
+        list_use_letter.add(write)
 
     if len(letter_list) == 0 :
         print("You survived!")
@@ -34,3 +41,6 @@ while True:
     elif guesses==0:
         print("You lost!")
         break
+
+print("""Thanks for playing!
+We'll see how well you did in the next stage""")
