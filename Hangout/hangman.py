6ef8e2495757acbfd3a6a03840961ffd17944b68
OlexandraSeveryn <o.y.severyn@student.khai.edu>
2021-12-08 00:36:22 +0200

    hangman 4-th stage

--- Hangout/hangman.py
+++ Hangout/hangman.py
@@ -6,8 +6,17 @@ The game will be available soon.""")
 word_list=["python", "java", "javascript"]
 
 word_select = random.choice(word_list)
-write = input("Guess the word > ")
+
+hint = ''
+for i in range(0,len(word_select)):
+    if i < 3:
+        hint +=word_select[i]
+    else :
+        hint +='-'
+
+write = input("Guess the word "+hint+"> ")
 if write == word_select:
     print("You survived!")
 else :
     print("You lost!")
+
