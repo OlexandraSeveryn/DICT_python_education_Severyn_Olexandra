c58e5bca2f3a7ec638d368e30178ab961a45cddd
OlexandraSeveryn <o.y.severyn@student.khai.edu>
2021-12-08 00:22:24 +0200

    hangman 3-rd stage

--- Hangout/hangman.py
+++ Hangout/hangman.py
@@ -1,9 +1,13 @@
+import random
+
 print("""HANGMAN
 The game will be available soon.""")
 
-word = "python"
+word_list=["python", "java", "javascript"]
+
+word_select = random.choice(word_list)
 write = input("Guess the word > ")
-if write == word:
+if write == word_select:
     print("You survived!")
 else :
     print("You lost!")
