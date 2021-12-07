b326cf29e21ef8f85b4374b7875702ae5849804a
OlexandraSeveryn <o.y.severyn@student.khai.edu>
2021-12-07 23:58:40 +0200

    hangman 2-nd stage

--- Hangout/hangman.py
+++ Hangout/hangman.py
@@ -1,2 +1,9 @@
 print("""HANGMAN
 The game will be available soon.""")
+
+word = "python"
+write = input("Guess the word > ")
+if write == word:
+    print("You survived!")
+else :
+    print("You lost!")
