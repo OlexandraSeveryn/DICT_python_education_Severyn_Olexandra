0e4a8e822075182af37d36ba99c6bdabe4ea95f4
OlexandraSeveryn <o.y.severyn@student.khai.edu>
2021-12-14 21:42:18 +0200

    CoffeeMachine 5-th stage

--- CoffeeMachine/coffeemachine.py
+++ CoffeeMachine/coffeemachine.py
@@ -24,63 +24,73 @@ def printStatus():
 
 printStatus()
 
+while True:
 
-print("Write action (buy, fill, take):")
-action = input(">")
+    print("Write action (buy, fill, take, remaining, exit):")
+    action = input(">")
 
-if action == "buy":
-    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-    type_coffee = int(input(">"))
+    if action == "buy":
+        print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back – to main menu:")
+        type_coffee = input(">")
 
-    coffee_max = min(available_water // 200, available_milk // 50, available_beans // 15)
+        if type_coffee == "back":
+            continue
+        else:
+            type_coffee = int(type_coffee)
 
-    water = milk = beans = price = 0
+        coffee_max = min(available_water // 200, available_milk // 50, available_beans // 15)
 
-    if type_coffee == 1:
-        water = 250
-        milk = 0
-        beans = 16
-        price = 4
-    elif type_coffee == 2:
-        water = 350
-        milk = 75
-        beans = 20
-        price = 7
-    elif type_coffee == 3:
-        water = 200
-        milk = 100
-        beans = 12
-        price = 6
+        water = milk = beans = price = 0
 
-    if water <= available_water and milk <= available_milk and beans <= available_beans:
+        if type_coffee == 1:
+            water = 250
+            milk = 0
+            beans = 16
+            price = 4
+        elif type_coffee == 2:
+            water = 350
+            milk = 75
+            beans = 20
+            price = 7
+        elif type_coffee == 3:
+            water = 200
+            milk = 100
+            beans = 12
+            price = 6
 
-        if coffee_max == 1:
-            print("Yes, I can make that amount of coffee")
-        elif coffee_max > 1:
-            print("Yes, I can make that amount of coffee (and even {} more than that)".format(coffee_max - 1))
+        if water <= available_water and milk <= available_milk and beans <= available_beans:
 
-        available_money += price
-        print_status()
+            if coffee_max == 1:
+                print("Yes, I can make that amount of coffee")
+            elif coffee_max > 1:
+                print("Yes, I can make that amount of coffee (and even {} more than that)".format(coffee_max - 1))
 
-    else:
-        print("No, I can make only {} cups of coffee".format(coffee_max))
+            available_money += price
+            printStatus()
 
-elif action =="fill":
-    print("Write how many ml of water you want to add:")
-    add_water = int(input(">"))
-    print("Write how many ml of milk you want to add:")
-    add_milk = int(input(">"))
-    print("Write how many grams of coffee beans you want to add:")
-    add_beans = int(input(">"))
-    print("Write how many disposable coffee cups you want to add:")
-    add_cups = int(input(">"))
+        else:
+            print("No, I can make only {} cups of coffee".format(coffee_max))
 
-    available_water += add_water
-    available_milk += add_milk
-    available_beans += add_beans
-    available_cup += add_cups
-    printStatus()
-elif action == "take":
-    print("I gave you {}".format(available_money))
-    available_money = 0
-    printStatus()
+    elif action =="fill":
+        print("Write how many ml of water you want to add:")
+        add_water = int(input(">"))
+        print("Write how many ml of milk you want to add:")
+        add_milk = int(input(">"))
+        print("Write how many grams of coffee beans you want to add:")
+        add_beans = int(input(">"))
+        print("Write how many disposable coffee cups you want to add:")
+        add_cups = int(input(">"))
+
+        available_water += add_water
+        available_milk += add_milk
+        available_beans += add_beans
+        available_cup += add_cups
+        printStatus()
+    elif action == "take":
+        print("I gave you {}".format(available_money))
+        availableMoney = 0
+        printStatus()
+    elif action == "remaining":
+        printStatus()
+    elif action == "exit":
+        break
