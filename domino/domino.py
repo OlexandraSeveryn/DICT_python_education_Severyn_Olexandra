6fd15f854111bb9ea55e2862f8f4b6b0f871e267
OlexandraSeveryn <o.y.severyn@student.khai.edu>
2021-12-24 02:25:31 +0200

    domino 4-th stage

--- domino/domino.py
+++ domino/domino.py
@@ -6,7 +6,6 @@ slice_stock = domino_pieces[:14]
 slice_player = domino_pieces[21:28]
 slice_computer = domino_pieces[14:21]
 tier_list = []
-
 while True:
     for i in slice_player:
         if i == i[::-1]:
@@ -24,10 +23,10 @@ else:
     m = tier_list
 if m in slice_player:
     slice_player.remove(m)
-    choice = cmp
+    choice = "Status: Computer is about to make a move. Press Enter to continue..."
 else:
     slice_computer.remove(m)
-    choice = pl
+    choice = "Status: It's your turn to make a move. Enter your command. "
 tier_list = [m]
 
 
@@ -35,30 +34,66 @@ def interface(m):
     print("=" * 70)
     print(f"""Stock pieces {len(slice_stock)}
 Computer pieces {len(slice_computer)}
-{m} 
- """)
-    print("""Your pieces:
+If you don't have a suitable dominance press 0 and you get 1 domino.
+""")
+    if len(m) <= 6:
+        print(m)
+    elif len(m) > 6:
+        print(f"""{m[:3]}...{m[-3:]}""")
+    print("""
+Your pieces:
        """)
     for x, item in enumerate(slice_player):
         print(f"{x + 1}: {item}")
 
 
-def player_input(player_answer):
+def check():
+    while True:
+        player_answer = input("Status: It's your turn to make a move. Enter your command. ")
+        try:
+            int(player_answer)
+            if int(player_answer) not in range(-len(slice_player), len(slice_player) + 1):
+                print("you don't have that much")
+                continue
+            else:
+                return player_answer
+        except ValueError:
+            print("Invalid input. Please try again.")
+            continue
+
+
+def player_input():
     turn = False
+    player_answer = int(check())
     while not turn:
         if player_answer < 0:
-            tier_list.insert(0, slice_player[-player_answer - 1])
-            slice_player.remove(slice_player[-player_answer - 1])
-            turn = True
+            if slice_player[abs(player_answer) - 1][-1] == tier_list[0][0]:
+                tier_list.insert(0, slice_player[-player_answer - 1])
+                slice_player.remove(slice_player[-player_answer - 1])
+                turn = True
+            elif slice_player[abs(player_answer) - 1][0] == tier_list[0][0]:
+                tier_list.insert(0, slice_player[-player_answer - 1][::-1])
+                slice_player.remove(slice_player[-player_answer - 1])
+                turn = True
+            else:
+                print("wrong one")
+                player_answer = int(check())
         elif player_answer > 0:
-            tier_list.append(slice_player[player_answer - 1])
-            slice_player.remove(slice_player[player_answer - 1])
-            turn = True
+            if slice_player[player_answer - 1][0] == tier_list[-1][1]:
+                tier_list.append(slice_player[player_answer - 1])
+                slice_player.remove(slice_player[player_answer - 1])
+                turn = True
+            elif slice_player[player_answer - 1][1] == tier_list[-1][1]:
+                tier_list.append(slice_player[player_answer - 1][::-1])
+                slice_player.remove(slice_player[player_answer - 1])
+                turn = True
+            else:
+                print("wrong one")
+                player_answer = int(check())
         elif player_answer == 0:
             slice_player.append(slice_stock[0])
             del slice_stock[0]
             turn = True
-
         else:
             print("Incorrect input. Enter number ")
             continue
@@ -67,16 +102,26 @@ def player_input(player_answer):
 def computer_input():
     turn = False
     while not turn:
-        computer_answer = random.choice(range(-len(slice_computer), len(slice_computer)+1))
+        computer_answer = random.choice(range(-len(slice_computer), len(slice_computer) + 1))
         if computer_answer < 0:
-            tier_list.insert(0, slice_computer[-computer_answer - 1])
-            slice_computer.remove(slice_computer[-computer_answer - 1])
-            turn = True
+            if slice_computer[abs(computer_answer) - 1][-1] == tier_list[0][0]:
+                tier_list.insert(0, slice_computer[abs(computer_answer) - 1])
+                slice_computer.remove(slice_computer[abs(computer_answer) - 1])
+                turn = True
+            elif slice_computer[abs(computer_answer) - 1][0] == tier_list[0][0]:
+                tier_list.insert(0, slice_computer[abs(computer_answer) - 1][::-1])
+                slice_computer.remove(slice_computer[abs(computer_answer) - 1])
+                turn = True
         elif computer_answer > 0:
-            tier_list.append((slice_computer[computer_answer - 1]))
-            slice_computer.remove(slice_computer[computer_answer - 1])
-            turn = True
-        elif computer_answer == 0:
+            if slice_computer[computer_answer - 1][0] == tier_list[-1][1]:
+                tier_list.append(slice_computer[computer_answer - 1])
+                slice_computer.remove(slice_computer[computer_answer - 1])
+                turn = True
+            elif slice_computer[computer_answer - 1][1] == tier_list[-1][1]:
+                tier_list.append(slice_computer[computer_answer - 1][::-1])
+                slice_computer.remove(slice_computer[computer_answer - 1])
+                turn = True
+        else:
             slice_computer.append(slice_stock[0])
             del slice_stock[0]
             turn = True
@@ -86,30 +131,17 @@ interface(tier_list)
 
 while True:
     if len(slice_computer) > 0 and len(slice_player) > 0:
-        if choice == cmp:
+        if choice == "Status: Computer is about to make a move. Press Enter to continue...":
+            print(input("Status: Computer is about to make a move. Press Enter to continue..."))
             computer_input()
-            choice = pl
+            choice = "Status: It's your turn to make a move. Enter your command"
         else:
-            move = input("Status: It's your turn to make a move. Enter your command. ")
-            try:
-                int(move)
-            except ValueError:
-                print("Invalid input. Please try again.")
-                continue
-            else:
-                if int(move) not in range(-len(slice_player), len(slice_player) + 1):
-                    print("you don't have that much")
-                    continue
-                if int(move) in range(-len(slice_computer), len(slice_computer)+1):
-                    player_input(int(move))
-                    choice = "Status: Computer is about to make a move. Press Enter to continue..."
-                else:
-                    print("Invalid input. Please try again.")
-                    continue
+            player_input()
+            choice = "Status: Computer is about to make a move. Press Enter to continue..."
     interface(tier_list)
     if len(slice_player) == 0:
         print("""The game is over. You won!
-        You have 0 pieces""")
+            You have 0 pieces""")
         break
     elif len(slice_computer) == 0:
         print("The game is over. The computer won!")
@@ -122,4 +154,11 @@ while True:
                     score += 1
         if score == 8:
             print("The game is over. It's a draw!")
+            break
+    elif len(slice_stock) == 0:
+        if choice == "Status: Computer is about to make a move. Press Enter to continue...":
+            print("The game is over. You won!")
+            break
+        elif choice == "Status: It's your turn to make a move. Enter your command":
+            print("The game is over. The computer won!")
             break
